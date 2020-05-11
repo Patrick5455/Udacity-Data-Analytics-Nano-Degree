01a77002009f7ce8a9e2f2b0dc99b9826f9dfab8
Patrick5455 <patrickojunde@gmail.com>
2020-05-11 12:38:46 +0100

    Upgraded binomial calculator to calculate probability

--- quiz_solutions_mine/binomial_distribution.py
+++ quiz_solutions_mine/binomial_distribution.py
@@ -5,25 +5,39 @@ def factorial(n):
 	return x
 
 def binomial(n, k):
-	n_factorial = factorial(n)
-	print("n! =",  n_factorial,"\n")
-	k_factorial = factorial(k)
-	print("k! = ", k_factorial,"\n")
-	k2_factorial = factorial(n-k)
-	print("k2! =", k2_factorial,"\n")
-	permutation = k_factorial * k2_factorial
-	print("permutation =", permutation,"\n")
-	probability = n_factorial/permutation
-	print("probability =", probability,"\n")
+	if k > n:
+		return ("events(k) cannot be greater than times(n)")
+	else:
+		n_factorial = factorial(n)
+		print("n! =",  n_factorial,"\n")
+		k_factorial = factorial(k)
+		print("k! = ", k_factorial,"\n")
+		k2_factorial = factorial(n-k)
+		print("k2! =", k2_factorial,"\n")
+		k_divisor = k_factorial * k2_factorial
+		print("k_divisor =", k_divisor,"\n")
+		distribution = n_factorial/k_divisor
+		print("distribution =", distribution,"\n")
+		return distribution
+
+def probability(n,k,v):
+	outcome = binomial(n,k)
+	v2=1-v
+	v_pow = k
+	v2_pow = n-k
+	total_v = (v**v_pow)*(v2**v2_pow)
+	probability = round(outcome*total_v,4)
+	
 	return probability
 	
 
-
+print("--- W e l c o m e  t o  P r o b a b i l i t y  C a l c u l a t o r ---")
 
 n = int(input("Enter n\n"))
 k = int(input("Enter k\n"))
+v = float(input("Enter v\n"))
 
-answer = binomial(n,k)
+answer = probability(n,k,v)
 
-print(answer)
+print("possible outocmes(s): ", answer)
 
